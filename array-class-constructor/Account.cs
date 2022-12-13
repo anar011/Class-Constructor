cd93e01b1cb053e44abd106a2ec43d084118d4de
anaraa@code.edu.az <you@example.com>
2022-12-13 15:06:58 -0800

    Class

---
+++ array-class-constructor/Account.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Security.Cryptography.X509Certificates;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace array_class_constructor
+//{
+//    public class Account
+//    {
+//        public string Username;
+//        public string Password;
+
+//        public Account(string username, string password)
+//        {
+//            Username = username;
+//            Password = password;    
+//        }
+
+//        public void Login()
+//        {
+//            string user = "cavid123";
+//            string pass = "Cavid1993";
+
+
+
+//            if (Username == user && Password == pass)
+//            {
+
+//                Console.WriteLine("Giris ugurludur");
+
+//            }
+//            else
+//            {
+//                Console.WriteLine("Username ve ya parol sehvdir");
+//            }
+
+//        }
+        
+        
+  
+//    }
+//}
---
+++ array-class-constructor/Program.cs
@@ -0,0 +1,242 @@
+﻿
+
+
+#region string
+
+
+//string name = "Pervin";
+//name = "Ferid";
+
+////////////////
+
+//using System.Text;
+
+
+//string name = "Per";
+//string name2 = "vin";
+//string endName = name + name2; 
+
+
+//StringBuilder stringBuilder = new StringBuilder();
+
+//stringBuilder.Append("Per");
+//stringBuilder.Append("vin" );
+
+//Console.WriteLine(stringBuilder);
+
+/////////////////////////////////////
+
+
+//string name = "Per";
+//string name2 = "vin";
+//string endName = name + name2;
+
+
+//StringBuilder stringBuilder = new StringBuilder("camal");
+
+
+//Console.WriteLine(stringBuilder);
+
+/////////////////////////
+//int[] arr2 = new int[10];
+
+//int[] arr3 = new int[5] { 1, 2, 3, 4, 5 };
+
+///////////////////
+
+//WriteConsole("hello");
+
+//int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+
+//Console.WriteLine(arr1[1]);
+
+//Console.WriteLine(arr1.Length);
+
+//Console.WriteLine(arr1.FirstOrDefault()) ;
+
+//Console.WriteLine(arr1[0]);
+
+
+//Array.Resize(ref arr1, 10);
+
+//Console.WriteLine(arr1[8]);
+
+
+//int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+
+
+//Console.WriteLine(arr1.Length);
+
+//Array.Resize(ref arr1, 10);
+
+
+//Console.WriteLine(arr1.Length);
+
+//int[] arr1 = { 1, 4, 3, 2, };
+
+
+
+//Array.Resize(ref arr1, 10);
+
+//arr1[9] = 100;
+
+//Console.WriteLine(arr1 [9]);
+
+
+//foreach(var item in arr1)
+//{
+//    Console.WriteLine(item);
+//}
+
+//Array.Sort(arr1);
+
+//foreach (var item in arr1)
+//{
+//    Console.WriteLine(item);
+//}
+
+//var newArr = arr1.Clone();
+
+//int[] arr4 = { 1, 2, 3 };
+
+//arr1.CopyTo(arr4, 2);
+
+
+//foreach (var item in arr1)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//Array.Reverse(arr1);
+
+//foreach (var item in arr1)
+//{
+//    Console.WriteLine(item);
+//}
+
+//string name = "Hesen";
+//var test = name.Reverse().ToArray();
+//string res = "";
+
+//foreach ( var item in test)
+//{
+//    res += item;
+//}
+//Console.WriteLine(res);
+
+//int[,] array2D = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+
+//Console.WriteLine(array2D[0,1]);
+/////////////////////////////////////////////
+
+
+//WriteConsole("salam");
+
+
+//string name = "Hesen";
+
+//string res = "";
+
+//Console.WriteLine(res);
+
+
+//ReverseStr(name);   
+//static void ReverseStr(string str)
+//{
+//    var reversedArray = str.Reverse().ToArray();
+
+//    foreach (var item in reversedArray)
+//    {
+//        Console.WriteLine(item);
+//    }
+//}
+//////////////////////////////////////////////////
+#endregion
+
+
+#region 
+
+
+//WriteConsole("sagol");
+
+//string name = "Hesen";
+
+//var result = ReverseStr(name);
+
+//Console.WriteLine(result);
+
+
+//static string ReverseStr(string str)
+//{
+//    var reversedArray = str.Reverse().ToArray();
+
+//    string res = "";
+
+//    foreach (var item in reversedArray)
+//    {
+//        res+= item;
+//    }
+
+
+//    return res;
+
+
+//}
+/////////////////////////////////
+
+
+//string name = "Hesen";
+
+//var result = ReverseStr(name);
+
+//ShowReversedstr(result);
+
+//Console.WriteLine(result);
+
+
+//static string ReverseStr(string str)
+//{
+//    var reversedArray = str.Reverse().ToArray();
+
+//    string res = "";
+
+//    foreach (var item in reversedArray)
+//    {
+//        res += item;
+//    }
+
+
+//    return res;
+
+
+//}
+
+//static void ShowReversedstr(string name)
+//{
+//    Console.WriteLine(name);
+//}
+
+//static void WriteConsole(string text)
+//{
+//    Console.WriteLine(text);
+//}
+
+//////////////////////////////////
+#endregion
+
+
+//using array_class_constructor;
+/////
+/*1) Account adinda class yaradib, onun icinde Login methodu yazin. Hemin login methoduna username ve 
+ * password gelecek. Username cavid123 ve password Cavid1993 -e beraberdirse ekrana giriw ugurludur 
+// * yazilsin, eks halda email ve ya password sehvdir cixsin*/
+
+
+//////////////////////////////////////////
+
+//using array_class_constructor;
+/////
+//Account acc = new Account("cavid123", "Cavid1993");
+
+//acc.Login();
